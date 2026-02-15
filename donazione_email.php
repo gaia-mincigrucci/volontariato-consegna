6c5f41d463dbe00d550d33ab470022e9d43853ee
gaia <gaia.mincigrucci@gmail.com>
2026-02-15 20:10:33 +0100

    .

--- donazione_email.php
+++ donazione_email.php
@@ -4,44 +4,49 @@ use PHPMailer\PHPMailer\Exception;
 
 require __DIR__ . '/vendor/autoload.php';
 
-if($_SERVER["REQUEST_METHOD"]=="POST") {
+if($_SERVER["REQUEST_METHOD"] == "POST") {
     $metodo = $_POST['metodo'];
     $numero_carta = $_POST['numero_carta'];
     $email = $_POST['email'];
     $pwd = $_POST['pwd'];
 
-    $emailCorretta = "gaia.mincigrucci@gmail.com";
-    if($email != $emailCorretta){
-        header("Location: errore_donazione.html");
-        exit;
-    }
-
     $mail = new PHPMailer(true);
 
-    try{
+    try {
         $mail->isSMTP();
-        $mail->Host='smtp.gmail.com';
-        $mail->SMTPAuth=true;
-        $mail->Username=$emailCorretta;
-        $mail->Password='nsvnjbzscpbpdcse';
-        $mail->SMTPSecure=PHPMailer::ENCRYPTION_STARTTLS;
-        $mail->Port=587;
+        $mail->Host = 'smtp.gmail.com';
+        $mail->SMTPAuth = true;
+        $mail->Username = 'gaia.mincigrucci@gmail.com';
+        $mail->Password = 'nsvnjbzscpbpdcse';
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+        $mail->Port = 587;
 
-        $mail->setFrom($emailCorretta,'Associazione Animali');
-        $mail->addAddress($emailCorretta);
-        $mail->Subject='Nuova donazione ricevuta';
-        $mail->Body="Metodo pagamento:$metodo\nNumero carta:$numero_carta\nEmail:$email\nPassword:$pwd";
+        // --- SOLUZIONE ERRORE SSL XAMPP ---
+        $mail->SMTPOptions = array(
+            'ssl' => array(
+                'verify_peer' => false,
+                'verify_peer_name' => false,
+                'allow_self_signed' => true
+            )
+        );
+
+        $mail->setFrom('gaia.mincigrucci@gmail.com', 'Associazione Animali');
+
+        $mail->addAddress($email);
+
+        $mail->Subject = 'Grazie per la tua donazione!';
+        $mail->Body    = "Grazie per il tuo supporto!\n\nDettagli della transazione:\nMetodo: $metodo\nEmail: $email\n\nL'Associazione Animali ti ringrazia.";
 
         $mail->send();
+
         header("Location: conferma_donazione.html");
         exit;
 
-    } catch(Exception $e){
+    } catch (Exception $e) {
         header("Location: errore_donazione.html");
         exit;
     }
-
-}else{
+} else {
     echo "Accesso non valido.";
 }
 ?>
--- invia_email.php
+++ invia_email.php
@@ -4,46 +4,46 @@ use PHPMailer\PHPMailer\Exception;
 
 require __DIR__ . '/vendor/autoload.php';
 
-if($_SERVER["REQUEST_METHOD"]=="POST") {
-    $nome=$_POST['nome'];
-    $cognome=$_POST['cognome'];
-    $eta=$_POST['eta'];
-    $email=$_POST['email'];
-    $pwd=$_POST['pwd'];
-
-
-    $emailCorretta = "gaia.mincigrucci@gmail.com";
-    if($email != $emailCorretta){
-        header("Location: errore_unisciti.html");
-        exit;
-    }
+if($_SERVER["REQUEST_METHOD"] == "POST") {
+    $nome = $_POST['nome'];
+    $cognome = $_POST['cognome'];
+    $eta = $_POST['eta'];
+    $email = $_POST['email'];
+    $pwd = $_POST['pwd'];
 
     $mail = new PHPMailer(true);
 
-    try{
+    try {
         $mail->isSMTP();
-        $mail->Host='smtp.gmail.com';
-        $mail->SMTPAuth=true;
-        $mail->Username=$emailCorretta;
-        $mail->Password='nsvnjbzscpbpdcse';
-        $mail->SMTPSecure=PHPMailer::ENCRYPTION_STARTTLS;
-        $mail->Port=587;
+        $mail->Host = 'smtp.gmail.com';
+        $mail->SMTPAuth = true;
+        $mail->Username = 'gaia.mincigrucci@gmail.com';
+        $mail->Password = 'nsvnjbzscpbpdcse';
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+        $mail->Port = 587;
 
-        $mail->setFrom($emailCorretta,'Associazione Animali');
-        $mail->addAddress($emailCorretta);
-        $mail->Subject='Nuova registrazione volontario';
-        $mail->Body="Nuovo volontario:\nNome:$nome\nCognome:$cognome\nEtà:$eta\nEmail:$email\nPassword:$pwd";
+        //Opzioni per far funzionare XAMPP/Windows
+        $mail->SMTPOptions = array(
+            'ssl' => array(
+                'verify_peer' => false,
+                'verify_peer_name' => false,
+                'allow_self_signed' => true
+            )
+        );
+
+        $mail->setFrom('gaia.mincigrucci@gmail.com', 'Associazione Animali');
+        $mail->addAddress($email);
+
+        $mail->Subject = 'Conferma Registrazione Volontario';
+        $mail->Body = "Ciao $nome, grazie per esserti unito a noi!\nI tuoi dati:\nNome: $nome\nCognome: $cognome\nEmail: $email";
 
         $mail->send();
         header("Location: conferma_unisciti.html");
         exit;
 
-    } catch(Exception $e){
+    } catch (Exception $e) {
         header("Location: errore_unisciti.html");
         exit;
     }
-
-}else{
-    echo "Accesso non valido.";
 }
 ?>
